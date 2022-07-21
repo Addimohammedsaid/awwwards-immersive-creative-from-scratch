db81f156602ce5015f45c03b2ed0bbbf43ecd27a
Addimohammedsaid <addi_mohammed_said@outlook.fr>
2022-07-22 08:22:49 +0900

    🚩  chore:  final webpack init configuration

---
+++ floema/app/index.js
@@ -0,0 +1,3 @@
+import image from "./images/itachi.jpeg";
+
+console.log("hello world");
--- floema/webpack.config.js
+++ floema/webpack.config.js
@@ -4,9 +4,13 @@ const webpack = require("webpack");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const CopyWebpackPlugin = require("copy-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const ImageMinimizerPlugin = require("image-minimizer-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
+
+const IS_DEVELOPMENT = process.env.NODE_ENV === "development";
 
 const dirApp = path.join(__dirname, "app");
-const dirShared = path.join(__dirname, "assets");
+const dirShared = path.join(__dirname, "shared");
 const dirStyles = path.join(__dirname, "styles");
 const dirNode = "node_modules";
 
@@ -14,5 +18,101 @@ module.exports = {
 	entry: [path.join(dirApp, "index.js"), path.join(dirStyles, "index.scss")],
 	resolve: {
 		modules: [dirNode, dirApp, dirShared, dirStyles],
+	},
+	plugins: [
+		new webpack.DefinePlugin({
+			IS_DEVELOPMENT,
+		}),
+		// copy plugin used to copy files from the public folder to the build folder
+		new CopyWebpackPlugin({
+			patterns: [
+				{
+					from: "./shared",
+					to: "",
+					noErrorOnMissing: true,
+				},
+			],
+		}),
+		// mini-css-extract-plugin used to extract css from the js files
+		new MiniCssExtractPlugin({
+			filename: "[name].css",
+			chunkFilename: "[id].css",
+		}),
+
+		new ImageMinimizerPlugin({
+			minimizer: {
+				implementation: ImageMinimizerPlugin.imageminMinify,
+				options: {
+					// Lossless optimization with custom option
+					// Feel free to experiment with options for better result for you
+					plugins: [
+						["gifsicle", { interlaced: true }],
+						["jpegtran", { progressive: true }],
+						["optipng", { optimizationLevel: 5 }],
+					],
+				},
+			},
+		}),
+
+		new CleanWebpackPlugin(),
+	],
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				use: {
+					loader: "babel-loader",
+				},
+			},
+			{
+				test: /\.scss$/,
+				use: [
+					{
+						loader: MiniCssExtractPlugin.loader,
+						options: {
+							publicPath: "",
+						},
+					},
+					{
+						loader: "css-loader",
+					},
+					{
+						loader: "sass-loader",
+					},
+				],
+			},
+			{
+				test: /\.(jpe?g|png|gif|svg|woff2?|fnt|webp)$/,
+				loader: "file-loader",
+				options: {
+					name(file) {
+						return "[hash].[ext]";
+					},
+				},
+			},
+			{
+				test: /\.(jpe?g|png|gif|svg|webp)$/i,
+				type: "asset",
+				// use: [
+				// 	{
+				// 		loader: ImageMinimizerPlugin.loader,
+				// 	},
+				// ],
+			},
+			{
+				test: /\.(glsl|frag|vert)$/,
+				loader: "raw-loader",
+				exclude: /node_modules/,
+			},
+			{
+				test: /\.(glsl|frag|vert)$/,
+				loader: "glslify-loader",
+				exclude: /node_modules/,
+			},
+		],
+	},
+	optimization: {
+		minimize: true,
+		minimizer: [new TerserPlugin()],
 	},
 };
