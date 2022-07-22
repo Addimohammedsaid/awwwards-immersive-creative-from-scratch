c292464172e64b24e637ff15f465c0c49b6e1d85
Addimohammedsaid <addi_mohammed_said@outlook.fr>
2022-07-23 06:59:37 +0900

    feature: implement pug render

--- floema/app.js
+++ floema/app.js
@@ -1,10 +1,22 @@
 const express = require("express");
 const app = express();
+const path = require("path");
+const port = 3000;
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.render("index", {
+    meta: {
+      data: {
+        title: "Home",
+        description: "Welcome to my website",
+      },
+    },
+  });
 });
 
-app.listen(3000, () => {
+app.listen(port, () => {
   console.log("Server is running on port 3000");
 });
