989ea938337d32e155336ffccebcd31bf9377021
Addimohammedsaid <addi_mohammed_said@outlook.fr>
2022-07-23 09:11:39 +0900

    chore: setup pug structure

--- floema/app.js
+++ floema/app.js
@@ -7,11 +7,44 @@ app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
 
 app.get("/", (req, res) => {
-  res.render("index", {
+  res.render("base", {
     meta: {
       data: {
         title: "Home",
         description: "Welcome to my website",
+      },
+    },
+  });
+});
+
+app.get("/about", (req, res) => {
+  res.render("pages/about", {
+    meta: {
+      data: {
+        title: "About",
+        description: "About me",
+      },
+    },
+  });
+});
+
+app.get("/details/:id", (req, res) => {
+  res.render("pages/details", {
+    meta: {
+      data: {
+        title: "Contact",
+        description: "Contact me",
+      },
+    },
+  });
+});
+
+app.get("/collections/:id", (req, res) => {
+  res.render("pages/collections", {
+    meta: {
+      data: {
+        title: "Collection",
+        description: "Collection of my products",
       },
     },
   });
