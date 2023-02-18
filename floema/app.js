6f84cc42b920211f42ed6475bd19cb98f87f3e96
Addimohammedsaid <addi_mohammed_said@outlook.fr>
2023-02-19 08:54:08 +0900

    ✨ make navigation dynamic with prismic api

--- floema/app.js
+++ floema/app.js
@@ -10,14 +10,27 @@ const app = express();
 const path = require("path");
 const port = 3000;
 
-const prismic = require("@prismicio/client");
-const PrismicDOM = require("prismic-dom");
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
 
 app.use(logger("dev"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(methodOverride());
 app.use(errorHandler());
+app.use((req, res, next) => {
+  // res.locals.ctx = {
+  //   endpoint: process.env.PRISMIC_ENDPOINT,
+  //   linkResolver: handleLinkResolver,
+  // };
+  res.locals.PrismicDOM = PrismicDOM;
+  res.locals.labels = ["one", "two", "three", "four"];
+  res.locals.Link = handleLinkResolver;
+  next();
+});
+
+const prismic = require("@prismicio/client");
+const PrismicDOM = require("prismic-dom");
 
 const initAPI = (req) => {
   return prismic.getApi(process.env.PRISMIC_ENDPOINT, {
@@ -27,30 +40,33 @@ const initAPI = (req) => {
 };
 
 const handleLinkResolver = (doc) => {
-  if (doc.type === "product") {
-    return `/detail/${doc.slug}`;
+  if (doc.link.type === "product") {
+    return `/detail/${doc.link.slug}`;
+  }
+  if (doc.link.type === "about") {
+    return `/about/`;
+  }
+  if (doc.link.type === "collections") {
+    return `/collections/`;
   }
   return "/";
 };
 
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "pug");
+const handleRequest = async (api) => {
+  const meta = await api.getSingle("metadata");
+  const preloader = await api.getSingle("preloader");
+  const navigation = await api.getSingle("navigation");
 
-app.use((req, res, next) => {
-  res.locals.ctx = {
-    endpoint: process.env.PRISMIC_ENDPOINT,
-    linkResolver: handleLinkResolver,
+  return {
+    meta,
+    navigation,
+    preloader,
   };
-  res.locals.PrismicDOM = PrismicDOM;
-  res.locals.labels = ["one", "two", "three", "four"];
-  res.locals.Link = handleLinkResolver;
-  next();
-});
+};
 
 app.get("/", async (req, res) => {
   const api = await initAPI(req);
-  const meta = await api.getSingle("metadata");
-  const preloader = await api.getSingle("preloader");
+  const defaults = await handleRequest(api);
   const home = await api.getSingle("home");
   const { results: collections } = await api.query(
     prismic.Predicates.at("document.type", "collection"),
@@ -61,41 +77,35 @@ app.get("/", async (req, res) => {
   res.render("pages/home", {
     collections,
     home,
-    meta,
-    preloader,
+    ...defaults,
   });
 });
 
 app.get("/about", async (req, res) => {
   const api = await initAPI(req);
-  const meta = await api.getSingle("metadata");
-  const preloader = await api.getSingle("preloader");
+  const defaults = handleRequest(api);
   const about = await api.getSingle("about");
   res.render("pages/about", {
     about,
-    meta,
-    preloader,
+    ...defaults,
   });
 });
 
 app.get("/detail/:uid", async (req, res) => {
   const api = await initAPI(req);
-  const meta = await api.getSingle("metadata");
-  const preloader = await api.getSingle("preloader");
+  const defaults = await handleRequest(api);
   const product = await api.getByUID("product", req.params.uid, {
     fetchLinks: "collection.title",
   });
   res.render("pages/detail", {
     product,
-    preloader,
-    meta,
+    ...defaults,
   });
 });
 
 app.get("/collections", async (req, res) => {
   const api = await initAPI(req);
-  const meta = await api.getSingle("metadata");
-  const preloader = await api.getSingle("preloader");
+  const defaults = await handleRequest(api);
   const home = await api.getSingle("home");
   const { results: collections } = await api.query(
     prismic.Predicates.at("document.type", "collection"),
@@ -106,8 +116,7 @@ app.get("/collections", async (req, res) => {
   res.render("pages/collections", {
     collections,
     home,
-    meta,
-    preloader,
+    ...defaults,
   });
 });
 
